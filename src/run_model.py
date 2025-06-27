fixed normalisation bug

src/run_model.py
@@ -295,16 +295,16 @@ def pnca_GCN_vary_graph(
         full_dataset.append(graph_dict['train'][sample]['graph'].dataset[0])
     for sample in graph_dict['test']:
         full_dataset.append(graph_dict['test'][sample]['graph'].dataset[0])
-    # !!!
-    # normalise, column wise for entire dataset
-    all_features = torch.cat([data.x for data in full_dataset], dim=0)
-    scaler = MinMaxScaler()
-    scaler.fit(all_features.numpy())
+    # # !!!
+    # # normalise, column wise for entire dataset
+    # all_features = torch.cat([data.x for data in full_dataset], dim=0)
+    # scaler = MinMaxScaler()
+    # scaler.fit(all_features.numpy())
 
-    # Apply normalization to each graph
-    for data in full_dataset:
-        data.x = torch.tensor(scaler.transform(data.x.numpy()), dtype=torch.float)
-    # !!!
+    # # Apply normalization to each graph
+    # for data in full_dataset:
+    #     data.x = torch.tensor(scaler.transform(data.x.numpy()), dtype=torch.float)
+    # # !!!
     # return full_dataset
 
     # Create DataLoaders for train and test set